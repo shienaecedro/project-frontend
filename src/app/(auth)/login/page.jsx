9fe8a33905b8b252fa986531d6066a10fc3132ca
Shienae Cedro <shienae.cedro@gmail.com>
2024-09-17 18:28:12 +0800

    updated login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,24 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import styles from './login.module.css';
 
 const LoginPage = () => {
     return (
-        <div className="loginContainer">
-            <h3>Login</h3>
+        <div className={styles.loginContainer}>
+            <h3 className={styles.loginHeader}>Login</h3>
             <form>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <label className="email">Email</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" />
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+
+                <button type="submit" className={`${styles.loginBtn} btn btn-primary`}>Login</button> {/* Apply both CSS module and Bootstrap class */}
             </form>
         </div>
     )
